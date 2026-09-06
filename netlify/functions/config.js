bd96700b6f282cbb2fed4ca58dae84c7ceb0dcc9
Logos6 <2089597761@qq.com>
2026-09-06 16:03:30 +0800

    fix: correct base64 encoded Supabase URL and key

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,11 +1,9 @@
 // Auto-generated config - DO NOT EDIT
-const https = require('https');
-
 const _b = (s) => Buffer.from(s, 'base64').toString('utf8');
 
 // Encoded fallback values (used when env vars not set)
-const _SUPABASE_URL_DEFAULT = 'aHR0cHM6Ly9odmRxcmVneWdrcGt6aWtpYmtwLnN1cGFiYXNlLmNv';
-const _SUPABASE_KEY_DEFAULT = 'c2Jfc2VjcmV0X19xcXlsZUtxWWU4SHQ2YXN3VFBTWXl3X3pZZTA0VF91';
+const _SUPABASE_URL_DEFAULT = 'aHR0cHM6Ly9odmRxaXZneWdrY3BremlraWJrcC5zdXBhYmFzZS5jbw==';
+const _SUPABASE_KEY_DEFAULT = 'c2Jfc2VjcmV0X19xeWxpS3VZZThIdDZhc3dUUFNZaXdfelllMDRUX3U=';
 
 function getConfig() {
   return {
